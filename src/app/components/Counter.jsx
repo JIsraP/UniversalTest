9770e93f2ff8a24185ed44544802d9fc36acc16b
JIsraP <a01423294@tec.mx>
2024-06-05 13:18:06 -0600

    refactored counter coding

--- src/app/components/Counter.jsx
+++ src/app/components/Counter.jsx
@@ -1,14 +1,8 @@
 import React, { useState } from 'react';
-import { Button, Typography, Grid, useMediaQuery } from '@mui/material';
-import { useTheme } from '@emotion/react';
+import { Button, Typography, Grid, Box, Container } from '@mui/material';
 
 export const Counter = () => {
     const [count, setCount] = useState(0);
-
-    const theme = useTheme();
-    const isLargeScreen = useMediaQuery(theme.breakpoints.up('lg'));
-    const isMediumScreen = useMediaQuery(theme.breakpoints.between('md', 'lg'));
-    const containerWidth = isLargeScreen ? '25vw' : isMediumScreen ? '35vw' : '50vw';
 
     const increment = () => {
         setCount(count + 1);
@@ -19,27 +13,29 @@ export const Counter = () => {
     };
 
     return (
-        <Grid container justifyContent='center' alignItems='center' boxShadow={2} width= { containerWidth } height='30vh' padding={5}>
-            <Grid item xs={12}>
-                <Typography variant="h6" gutterBottom>
-                    Counter
-                </Typography>
-                <Typography variant="h3" gutterBottom>
-                    {count}
-                </Typography>
-            </Grid>
-            <Grid container spacing={2} justifyContent="center">
-                <Grid item>
-                    <Button variant="contained" color="secondary" onClick={decrement}>
-                        -
-                    </Button>
+        <Container maxWidth="xs">
+            <Box boxShadow={2} padding={5}>
+                <Grid>
+                    <Typography variant="h6" gutterBottom>
+                        Counter
+                    </Typography>
+                    <Typography variant="h3" gutterBottom>
+                        {count}
+                    </Typography>
                 </Grid>
-                <Grid item>
-                    <Button variant="contained" color="primary" onClick={increment}>
-                        +
-                    </Button>
+                <Grid container spacing={2} justifyContent="center">
+                    <Grid item>
+                        <Button variant="contained" color="secondary" onClick={decrement}>
+                            -
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        <Button variant="contained" color="primary" onClick={increment}>
+                            +
+                        </Button>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Grid>
+            </Box>
+        </Container>
     );
 };
